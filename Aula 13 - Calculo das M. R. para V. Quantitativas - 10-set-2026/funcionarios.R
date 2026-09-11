124c6948c1d6da4d2e3d381edc376623b5c2926b
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2026-09-11 09:54:42 -0300

    :sparkles: feat: "Aula 13 - Calculo das M. R. para V. Quantitativas - 10-set-2026"

---
+++ Aula 13 - Calculo das M. R. para V. Quantitativas - 10-set-2026/funcionarios.R
@@ -0,0 +1,14 @@
+mean(funcionarios$salario)
+mean(funcionarios$idade_anos)
+
+median(funcionarios$salario)
+median(funcionarios$idade_anos)
+
+var(funcionarios$salario)
+var(funcionarios$idade_anos)
+
+sd(funcionarios$salario)
+sd(funcionarios$idade_anos)
+
+sd(funcionarios$salario) / mean(funcionarios$salario) * 100
+sd(funcionarios$idade_anos) / mean(funcionarios$idade_anos) * 100
